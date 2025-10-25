6f756ec1a7c535c410377e3c95a61c0c43d7ee61
ange-r <angeles.rechach@gmail.com>
2025-10-25 19:28:44 -0300

    Completo contrat HelloContract

--- Stellar/Entregas/Semana-2_Soroban/hello-tiburona/contracts/hello-world/src/lib.rs
+++ Stellar/Entregas/Semana-2_Soroban/hello-tiburona/contracts/hello-world/src/lib.rs
@@ -27,10 +27,62 @@ pub struct HelloTiburonaContract;
 
 
 #[contractimpl]
-impl HelloTiburonaContract {
-    pub fn hello_tiburona(env: Env, to: String) -> Vec<String> {
-        vec![&env, String::from_str(&env, "¡Hola Tiburona!"), to]
-    }
-}
+#[contract]
+pub struct HelloContract;
 
-mod test;
+#[contractimpl]
+impl HelloContract {
+    pub fn initialize(env: Env, admin: Address) -> Result<(), Error> {
+        // Antes de instanciar, verificamos si ya existe un admin
+        if env.storage().instance().has(&DataKey::Admin) {
+            return Err(Error::NoInicializado);
+        }
+        
+        env.storage()
+            .instance()
+            .set(&DataKey::Admin, &admin);
+        
+        env.storage()
+            .instance()
+            .set(&DataKey::ContadorSaludos, &0u32);
+        
+        env.storage()
+            .instance()
+            .extend_ttl(100, 100);
+        
+        Ok(())
+    }
+    
+    pub fn hello(env: Env, usuario: Address, nombre: String) -> Result<Symbol, Error> {
+        // Primero validar que el dato no este vacio
+        if nombre.len() == 0 {
+            return Err(Error::NombreVacio);
+        }
+        // Validar que el dato no desborde el tipo de dato
+        if nombre.len() > 32 {
+            return Err(Error::NombreMuyLargo);
+        }
+        
+        // Leer, modificar y guardar contador de saludos
+        let key_contador = DataKey::ContadorSaludos;
+        
+        let contador: u32 = env.storage()
+            .instance()
+            .get(&key_contador)
+            .unwrap_or(0);
+        
+        env.storage()
+            .instance()
+            .set(&key_contador, &(contador + 1));
+
+        // Estender TTL para poder guardar los datos
+        env.storage()
+            .persistent()
+            .extend_ttl(&DataKey::UltimoSaludo(usuario), 100, 100);
+        
+        env.storage()
+            .instance()
+            .extend_ttl(100, 100);
+    }
+    Ok(Symbol::new(&env, "Hola Tiburona!"))
+}
